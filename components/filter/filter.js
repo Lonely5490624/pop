cea5c0fa979f78be071b0235830829c43898a637
Lonely <l5490624@126.com>
2018-11-05 22:54:49 +0800

    wode

--- components/filter/filter.js
+++ components/filter/filter.js
@@ -11,27 +11,9 @@ Component({
    * 组件的初始数据
    */
   data: {
-    low: 0,
-    high: 100
-  },
-
-  onload: function() {
 
   },
 
-  lowValueChangeAction: function (e) {
-    console.log(e)
-    this.setData({
-      low: e.detail.lowValue
-    })
-  },
-
-  highValueChangeAction: function (e) {
-    console.log(e)
-    this.setData({
-      high: e.detail.highValue
-    })
-  },
   /**
    * 组件的方法列表
    */
