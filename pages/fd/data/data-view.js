88ea14a3cd59c4503abd1baef25f96e49076a73c
l5490624@126.com <LIlong5490624>
2018-11-19 22:53:30 +0800

    图表

--- pages/fd/data/data-view.js
+++ pages/fd/data/data-view.js
@@ -1,5 +1,7 @@
 import * as echarts from '../../../ec-canvas/echarts';
 const app = getApp();
+var xData = []
+var yData = []
 function initChart(canvas, width, height) {
   const chart = echarts.init(canvas, null, {
     width: width,
@@ -29,7 +31,7 @@ function initChart(canvas, width, height) {
     xAxis: {
       type: 'category',
       boundaryGap: false,
-      data: app.globalData.yData,
+      data: yData,
       //data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
       // show: false
     },
@@ -48,7 +50,7 @@ function initChart(canvas, width, height) {
       type: 'line',
       smooth: false,
       //data: [18, 36, 65, 30, 78, 40, -1]//y轴
-      data: app.globalData.xData
+      data: xData
     }]
   };
 
@@ -67,9 +69,9 @@ Page({
   },
   data: {
     viewInfo: [],
-    // ec: {
-    //   onInit: initChart
-    // }
+    ec: {
+      onInit: initChart
+    }
   },
 
   onLoad: function(options) {
@@ -77,18 +79,28 @@ Page({
       member_type: '2',
       space_id: ''
     }
+    this.ecComponent = this.selectComponent('#mychart-dom-line')
     app.http('/data/browse_data', data)
       .then(res => {
+        xData = [18, 36, 65, 30, 78, 40, -1]
+        yData = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        this.ecComponent.init((canvas, width, height) => {
+          const chart = echarts.init(canvas, null, {
+            width: width,
+            height: height
+          });
+          this.setOption(chart);
+          this.chart = chart;
+          return chart;
+        });
+
         this.setData({
           viewInfo: res.data
-        })  
+        })
         console.log(this.data.viewInfo)
         getApp().globalData.xData = [18, 36, 65, 30, 78, 40, -1];
         getApp().globalData.yData = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
         console.log(app.globalData.xData)
-        //xData = [18, 36, 65, 30, 78, 40, -1]
-        //yData = res.data.month_arr
-        //yData = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
         //console.log(xData)
         //console.log(yData)
         //initChart(canvas, '500prx', '500rpx')
@@ -96,10 +108,58 @@ Page({
       })
     // console.log(xData)
     //console.log(app.globalData.xData)
-    this.setData({
-      ec: {
-        onInit: initChart
-      }
-    })
+    // this.setData({
+    //   ec: {
+    //     onInit: initChart
+    //   }
+    // })
+  },
+  setOption (chart) {
+    const option = {
+      title: {
+        text: '',
+        left: 'center'
+      },
+      color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
+      // legend: {
+      //   data: ['A', 'B', 'C'],
+      //   top: 50,
+      //   left: 'center',
+      //   backgroundColor: 'red',
+      //   z: 100
+      // },
+      grid: {
+        containLabel: true
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis'
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: yData,
+        //data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        // show: false
+      },
+      yAxis: {
+        x: 'center',
+        type: 'value',
+        splitLine: {
+          lineStyle: {
+            type: 'dashed'
+          }
+        }
+        // show: false
+      },
+      series: [{
+        name: 'A',
+        type: 'line',
+        smooth: false,
+        //data: [18, 36, 65, 30, 78, 40, -1]//y轴
+        data: xData
+      }]
+    }
+    chart.setOption(option)
   }
 })
